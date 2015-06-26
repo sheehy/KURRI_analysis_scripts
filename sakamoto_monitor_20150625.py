3813dfd5f863ec0ffb0a2dc6618db0de39ee4e14
sheehy <suziesheehy@gmail.com>
2015-06-26 02:05:44 +0100

    Updated Shinji's script to debug polynomial order

--- sakamoto_monitor_20150625.py
+++ sakamoto_monitor_20150625.py
@@ -20,11 +20,11 @@ tdat2, data2=read_scope(fdirect, fname2)
 rtdat, rvdat=read_scope(fdirect, bunchmonf)
 
 #choose index range over which to analyse data (the initial stuff is probably rubbish!)
-starti = 90000
-endi=400000
+starti = 300000
+endi=300100
 plot(rtdat[starti:endi], rvdat[starti:endi], 'r-')
-#plot(tdat[starti:endi], data1[starti:endi], 'b-')
-#plot(tdat2[starti:endi], data2[starti:endi], 'c-')
+plot(tdat[starti:endi], abs(numpy.array(data1[starti:endi])), 'b-')
+plot(tdat2[starti:endi], abs(numpy.array(data2[starti:endi])), 'g-')
 show()
 
 filter=True
@@ -37,9 +37,9 @@ if filter==True:
 	#apply high pass filter
 	filter_chf1 = highpass(data1,dt,RC)
 	filter_chf2 = highpass(data2,dt,RC)
-	plot(tdat[starti:endi], filter_chf1[starti:endi], 'r-')
-	plot(tdat2[starti:endi], filter_chf2[starti:endi], 'b-')
-	show()
+	#plot(tdat[starti:endi], filter_chf1[starti:endi], 'r-')
+	#plot(tdat2[starti:endi], filter_chf2[starti:endi], 'b-')
+	
 
 	data1=filter_chf1
 	data2=filter_chf2
@@ -49,28 +49,39 @@ Lpeakdat = find_peaks(tdat[starti:endi], data1[starti:endi], interval, 0)
 Rpeakdat = find_peaks(tdat2[starti:endi], data2[starti:endi], interval, 0)
 
 #plot(tdat[starti:endi], data1[starti:endi], 'b-')
-plot(Lpeakdat[1], Lpeakdat[2], 'r.')
-plot(Rpeakdat[1], Rpeakdat[2], 'b.')
+plot(tdat[starti:endi], abs(numpy.array(data1[starti:endi])), 'r-')
+plot(tdat2[starti:endi], abs(numpy.array(data2[starti:endi])), 'b-')
+#plot(Lpeakdat[1], Lpeakdat[2], 'r.-')
+#plot(Rpeakdat[1], Rpeakdat[2], 'b.-')
 show()
 #sys.exit()
+
 #take sum and difference
 sumdat=[]
 diffdat=[]
 tdata=[]
+
 #find where peak indices correspond in the data (ie. L and R had same peak time, so beam actually went past at same time)
 #then do sum/difference of those peaks
-for pk in Lpeakdat[0]:
-	if pk in Rpeakdat[0]:
-		tdata.append(tdat[starti+pk])
-		sumdat.append(Lpeakdat[2][pk]+Rpeakdat[2][pk])
-		diffdat.append(Lpeakdat[2][pk]-Rpeakdat[2][pk])
+print len(Lpeakdat[0])
+print len(Rpeakdat[0])
+
+for pkindex in Lpeakdat[0]:
+	if Lpeakdat[1][pkindex] in Rpeakdat[1]:
+		print Lpeakdat[1][pkindex], Rpeakdat[1][pkindex]
+		tdata.append(Lpeakdat[1][pkindex])
+		sumdat.append(Lpeakdat[2][pkindex]+Rpeakdat[2][pkindex])
+		diffdat.append(Lpeakdat[2][pkindex]-Rpeakdat[2][pkindex])
 
 #normalise to find position
 divdat=numpy.array(diffdat)/numpy.array(sumdat)
 print len(sumdat), len(diffdat)
 print len(divdat)
-print len(tdata)
-
+print Rpeakdat[1][:10]
+print Lpeakdat[1][:10]
+print tdat[:10]
+print tdata[:10]
+sys.exit()
 #plot position vs time
 plot(tdata, sumdat, 'c-')
 plot(tdata, diffdat, 'm-')
